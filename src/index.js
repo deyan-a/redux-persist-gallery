174d68b12538e786c7f388a6ae9b866795dca33d
Deyan <deyan-a@protonmail.com>
2020-02-23 14:56:41 +0200

    wrapped app in BrowserRouter component

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import {store, persistor } from './store';
 import Mainlayout from './components/layouts/MainLayout';
 
@@ -9,9 +10,11 @@ import './index.css';
 
 ReactDOM.render(
     <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <Mainlayout />
-        </PersistGate>
+        <Router>
+            <PersistGate loading={null} persistor={persistor}>
+                <Mainlayout />
+            </PersistGate>
+        </Router>
     </Provider>,
     document.getElementById('root')
 );
