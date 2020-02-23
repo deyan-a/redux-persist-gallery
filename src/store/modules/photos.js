c18833de81784b6fc09258b4482a7fb6dc46c3ec
Deyan <deyan-a@protonmail.com>
2020-02-23 17:50:58 +0200

    added actions and actions creators for adding/removing photos from favorites

--- src/store/modules/photos.js
+++ src/store/modules/photos.js
@@ -2,6 +2,8 @@ import { PhotosApi } from '../../api';
 import PhotosService from '../../utils/services/PhotosService';
 
 export const GET_ALL_PHOTOS = 'GET_ALL_PHOTOS';
+export const ADD_TO_FAVORITES = 'ADD_TO_FAVORITES';
+export const REMOVE_FROM_FAVORITES = 'REMOVE_FROM_FAVORITES';
 
 const DEFAULT_STATE = {
     albums: {},
@@ -16,10 +18,34 @@ export default function(state = DEFAULT_STATE, action) {
         return PhotosService.mergePhotos(state, response);
     }
 
+    if (action.type === ADD_TO_FAVORITES) {
+        return {
+            ...state,
+            favorites: [...state.favorites, action.payload]
+        };
+    }
+
+    if (action.type === REMOVE_FROM_FAVORITES) {
+        return {
+            ...state,
+            favorites: [...state.favorites.filter(id => id !== action.payload)]
+        };
+    }
+
     return state;
 }
 
 export const getAllPhotos = () => ({
     type: GET_ALL_PHOTOS,
     payload: PhotosApi.getAllPhotos()
+});
+
+export const addToFavorites = photoId => ({
+    type: ADD_TO_FAVORITES,
+    payload: photoId
+});
+
+export const removeFromFavorites = photoId => ({
+    type: REMOVE_FROM_FAVORITES,
+    payload: photoId
 });
