8937757fe26d7351b11890c57b9a799f65f2cf2c
Deyan <deyan-a@protonmail.com>
2020-02-23 10:56:27 +0200

    added redux-persist to the store initialization

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promise from 'redux-promise-middleware';
+import { persistStore } from 'redux-persist';
 import rootReducer from './modules';
 
 const middleware = applyMiddleware(promise, thunk);
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-export default createStore(rootReducer, composeEnhancers(middleware));
+const store = createStore(rootReducer, composeEnhancers(middleware));
+const persistor = persistStore(store);
+
+export default { store, persistor };
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,4 +1,12 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import photos from './photos';
 
-export default combineReducers({ photos });
+const persistConfig = {
+    key: 'root',
+    storage
+};
+const rootReducer = combineReducers({ photos });
+
+export default persistReducer(persistConfig, rootReducer);
