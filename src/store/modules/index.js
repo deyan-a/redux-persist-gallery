32b6f747ba71c9ad946fdc9c0967bf968ebd4311
Deyan <deyan-a@protonmail.com>
2020-02-23 13:32:31 +0200

    added status to rootReducer init, also added a whitelist property to persistConfig, so that only 'photos' state is persisted

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import photos from './photos';
+import status from './status';
 
 const persistConfig = {
     key: 'root',
-    storage
+    storage,
+    whitelist: ['photos']
 };
-const rootReducer = combineReducers({ photos });
+const rootReducer = combineReducers({ photos, status });
 
 export default persistReducer(persistConfig, rootReducer);
