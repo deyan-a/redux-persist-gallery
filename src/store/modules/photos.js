3971fdab9667fd4e0b202251823015966317d1f0
Deyan <deyan-a@protonmail.com>
2020-02-23 14:26:25 +0200

    normalized fetched data

--- src/store/modules/photos.js
+++ src/store/modules/photos.js
@@ -3,14 +3,69 @@ import { PhotosApi } from '../../api';
 export const GET_ALL_PHOTOS = 'GET_ALL_PHOTOS';
 
 const DEFAULT_STATE = {
-    albums: null,
-    photos: null,
-    allPhotos: []
+    albums: {},
+    photos: {},
+    favorites: []
 };
+
+
+const pipe = (...fns) => (state, data) => fns.reduce((res, f) => f(res, data), state);
+
+const addPhotoToAlbum = (state, photo) => {
+    const { albumId, id } = photo;
+
+    
+    if (!state.albums[albumId]) {
+        return {
+            ...state,
+            albums: {
+                ...state.albums,
+                [albumId]: [id]
+            }
+        };
+    }
+
+    if(state.albums[albumId].includes(id)) {
+        return state;
+    }
+
+    return {
+        ...state,
+        albums: {
+            ...state.albums,
+            [albumId]: [...state.albums[albumId], id]
+        }
+    };
+};
+
+const addNewPhoto = (state, photo) => {
+    if (state.photos[photo.id]) {
+        return {
+            ...state,
+            photos: {
+                ...state.photos,
+                [photo.id]: {
+                    ...state.photos[photo.id],
+                    ...photo
+                }
+            }
+        };
+       }
+
+        return {...state,
+            photos: {
+                ...state.photos, 
+                [photo.id]: photo
+            }
+        };
+};
+
+const photosPipe = pipe(addNewPhoto, addPhotoToAlbum);
 
 export default function(state = DEFAULT_STATE, action) {
     if (action.type === `${GET_ALL_PHOTOS}_FULFILLED`) {
-        return state;
+        const response = action.payload.data;
+        return response.reduce((res, photo) => photosPipe(res, photo), state);
     }
 
     return state;
